3b19427b115f699bad766db7372d54fbb31c5e1f
ZaydKudaimi <107939751+ZaydKudaimi@users.noreply.github.com>
2023-03-27 01:05:37 -0700

    Update LinkStateSimulator.java

--- LinkStateSimulator.java
+++ LinkStateSimulator.java
@@ -1,7 +1,31 @@
+package linkstate;
+
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ *
+ * @author zayd
+ */
 public class LinkStateSimulator {
-    public static void main(String args[]) {
-    
 
-
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        // TODO code application logic here
+        File topology = new File(args[0]);
+        FileReader f = new FileReader(topology);
+        char c = (char) f.read();
+        String s = "";
+        while(c != (char)-1){
+            s += c;
+            c = (char) f.read();
+        }
     }
+    
 }
