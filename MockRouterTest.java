1368c86d63ae8270ba2a447a4def2e2ea09e85b9
ZaydKudaimi <107939751+ZaydKudaimi@users.noreply.github.com>
2023-03-27 22:35:37 -0700

    Update MockRouterTest.java

--- MockRouterTest.java
+++ MockRouterTest.java
@@ -1,9 +1,9 @@
 // A test class for MockRouter.  Starts 3 MockRouters on ports 9990/9991/9992 connected in a line.  9990-9991-9992 all with distance of 1
 public class MockRouterTest {
     public static void main(String args[]) {
-        MockRouter mr1 = new MockRouter(9990,"9991-1");
-        MockRouter mr2 = new MockRouter(9991,"9990-1 9992-1");
-        MockRouter mr3 = new MockRouter(9992,"9991-1");
+        MockRouter mr1 = new MockRouter(9990,"9991-1".split(" "));
+        MockRouter mr2 = new MockRouter(9991,"9990-1 9992-1".split(" "));
+        MockRouter mr3 = new MockRouter(9992,"9991-1".split(" "));
 
         // Start all the server(listener) threads
         new Thread(mr1.Listener).start();
