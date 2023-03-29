640738599d2889fbf6049ee744b71946d2bea723
Steve Rubin <ser@tch.org>
2023-03-28 21:33:48 -0700

    track seq #, but there there is bug and it doesnt work right

--- MockRouter.java
+++ MockRouter.java
@@ -8,6 +8,7 @@ import java.net.*;
 import java.util.regex.Pattern;
 import java.io.*;
 import java.util.LinkedList;
+
 import java.util.*;
 import java.time.*;
 
@@ -26,12 +27,12 @@ public class MockRouter {
         System.out.println("In the construtor");
         long startTime = Instant.now().toEpochMilli();
 
+        LinkedList<LSP> listLSP = new LinkedList<LSP>();
 
 
         Listener = new Runnable() {           // SERVER CODE GOES HERE
             ServerSocket ss;
             Socket s;
-            LinkedList<LSP> listLSP = new LinkedList<LSP>();
             public void run()  { 
                 try {
                      ss = new ServerSocket(portNumber);
@@ -41,13 +42,13 @@ public class MockRouter {
                     return;
                 }
                 while (keepRunning) {
-                    System.out.printf("Port %d waiting on accept()\n",portNumber);
+                    //System.out.printf("Port %d waiting on accept()\n",portNumber);
                     try {
                         s = ss.accept();
                     }
                     catch (Exception IOException) {
                     }
-                    System.out.printf("Port %d accepted connection from %s:%d\n",portNumber,s.getInetAddress().getHostAddress(),s.getPort());
+                    System.out.printf("[%d] accepted connection from %s:%d\n",portNumber,s.getInetAddress().getHostAddress(),s.getPort());
                     try { 
                         BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream())); 
                         PrintWriter pw = new PrintWriter(s.getOutputStream(), true);
@@ -59,50 +60,53 @@ public class MockRouter {
                          * (c) s followed by a newline (stop) to which it responds with STOPPING and a newline and then it stops its thread. 
                         */
                         if (command.charAt(0) == 'k') {  // Keepalive.  Just just a do-nothing to verify the connect/accept/read/write was working, sends a k back.
-                            System.out.printf("Port %d:%s:%d - RX Keepalive\n",portNumber,s.getInetAddress().getHostAddress(),s.getPort());      
+                            System.out.printf("[%d] %s:%d - RX Keepalive\n",portNumber,s.getInetAddress().getHostAddress(),s.getPort());      
                             pw.println("k");
                             pw.flush();  
                         }    
                         // l sender_port seq_number time_to_live adjacent_router_1_port-distance_1 ... adjacent_router_n_port-distance_n
                         // Stores the links-distance pairs into 2 arrays, and then puts those in a record class called LSP, those LSPs are then put in a list called listLSP;
-                        if (command.charAt(0) == 'l') {  // Received a link state message *TODO*
+                        if (command.charAt(0) == 'l') {  // Received a link state message
                             String chunks[] = command.split(" ");
                             //int [] adjRouterPorts = new int[100];
                             //int [] distances = new int[100];
+                            boolean dupSeq = false;
                             ArrayList<Integer> adjRouterPorts = new ArrayList<Integer>(100);
                             ArrayList<Integer> distances = new ArrayList<Integer>(100);
 
                             for (int x=4; x < chunks.length; x++) {
                                 String[] adjRouterPortDistance = chunks[x].split("-");
-                                System.out.printf("%s %s %s %s\n",chunks[0],chunks[1],chunks[2],chunks[3]);
-                                System.out.printf("%s - %s\n",adjRouterPortDistance[0],adjRouterPortDistance[1]);
+                               // System.out.printf("%s %s %s %s\n",chunks[0],chunks[1],chunks[2],chunks[3]);
+                               // System.out.printf("%s - %s\n",adjRouterPortDistance[0],adjRouterPortDistance[1]);
                                 adjRouterPorts.add(Integer.parseInt(adjRouterPortDistance[0]));
                                 distances.add(Integer.parseInt(adjRouterPortDistance[1]));
                             }   
+                            for (LSP lspdb : listLSP) {
+                            // Find existing LSP for router
+                                if (lspdb.senderPort() == Integer.parseInt(chunks[1]))
+                                    if (lspdb.seq() >= Integer.parseInt(chunks[2])) {
+                                        dupSeq = true;
+                                        continue;// if found, see if sequence number is newer
+                                    }           
+                            }
 
-                        // Find existing LSP for router
-                        // see if sequence number is newer
-                        // need to store the time received too
-                        // TTL is decremented on storing the lsp
-                        int ttl = Integer.parseInt(chunks[3])-1;
-                        if (ttl >= 1) { 
-                            LSP lsp = new LSP(Instant.now().toEpochMilli()-startTime,Integer.parseInt(chunks[1]),Integer.parseInt(chunks[2]),Integer.parseInt(chunks[3]),adjRouterPorts,distances);
-                            listLSP.add(lsp);
-                        } 
-                        else { 
+      
+                            int ttl = Integer.parseInt(chunks[3]);
+                            if (ttl >= 0 && dupSeq == false) { 
+                                LSP lsp = new LSP(Instant.now().toEpochMilli()-startTime,Integer.parseInt(chunks[1]),Integer.parseInt(chunks[2]),Integer.parseInt(chunks[3]),adjRouterPorts,distances);
+                                listLSP.add(lsp);
+                            } 
+                            else { 
 
-                        /* 
-                        The TTL (aka Age) field is decremented once per second by the routers who are remembering a particular router, sequence number pair, 
-                        if it hits zero, the router discards the entry. They also reflood this LSP with a TTL of 0. This signals to other router to delete the entry.
-                        */
-                        }
-                        pw.println("ACK");
-                        pw.flush();
+                           
+                            }
+                            pw.println("ACK");
+                            pw.flush();
 
                         }
 
                         if (command.charAt(0) == 'h') {           // Dump all link state messages and routing table *TODO*
-                            System.out.printf("Port %d:%s:%d - RX HISTORY\n",portNumber,s.getInetAddress().getHostAddress(),s.getPort());  
+                            //System.out.printf("Port %d:%s:%d - RX HISTORY\n",portNumber,s.getInetAddress().getHostAddress(),s.getPort());  
                             for (LSP lsp : listLSP) {
                                 pw.printf("T+%s %d %d %d", Long.toUnsignedString(lsp.time()),lsp.senderPort(),lsp.seq(),lsp.ttl());
                                 for (int x=0; x < lsp.adjRouterPort().size(); x++) {
@@ -115,7 +119,7 @@ public class MockRouter {
 
                         }
                         if (command.charAt(0) == 's') {           // STOP *DONE?*
-                            System.out.printf("Port %d:%s:%d - RX STOP\n",portNumber,s.getInetAddress().getHostAddress(),s.getPort());                       
+                            //System.out.printf("Port %d:%s:%d - RX STOP\n",portNumber,s.getInetAddress().getHostAddress(),s.getPort());                       
                             pw.println("STOPPING");
                             pw.flush();
                             s.close();
@@ -152,33 +156,57 @@ public class MockRouter {
             public void run() {             // CLIENT CODE GOES HERE
                 int port;
                 int distance;
-
+                
+                
+                Hashtable<Integer,Integer> seqAck = new Hashtable<Integer,Integer>();
                 while (keepRunning) {
                     float sleepyTime=(float) ((Math.random()*1000)+3000);
 
                     try { 
-                        System.out.printf("Client sleeping for %f\n",sleepyTime);
+                        //System.out.printf("Client sleeping for %f\n",sleepyTime);
                         Thread.sleep((long)sleepyTime);
                     } catch (Exception ignore) {};
                     String[] endpoints = adjacents;
                     for (String e: endpoints) {     // Main outgoing connection loop here
-                     String ep[] = e.split("-");
-                     System.out.printf("Thread %d connecting to Thread %s and Distance %s\n", portNumber,ep[0],ep[1]);
-                     port = Integer.parseInt(ep[0]);
-                     distance = Integer.parseInt(ep[1]);
+                        String ep[] = e.split("-");
+                        System.out.printf("[%d] connecting to Thread %s and Distance %s\n", portNumber,ep[0],ep[1]);
+                        port = Integer.parseInt(ep[0]);
+                        distance = Integer.parseInt(ep[1]);
                         try {
                           Socket s = new Socket("localhost", port);
                            BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream())); 
                            PrintWriter pw = new PrintWriter(s.getOutputStream(), true);
                            // do some stuff here with the remote connection
-                           pw.println("k"); // just send a keepalive for now, needs to come out when we do the link state packet part
-                           pw.flush();
-                                if (br.readLine().charAt(0) == 'k') { 
-                                    System.out.printf("Received Keepalive back from %d\n",port);
-                                }
-                                else {
-                                    System.out.printf("");
-                                }
+                          // loop through lsp list, if there is a lsp, and the seq number is greater than the seqAck then send it
+                          for (LSP lsp : listLSP) {
+                            System.out.printf("[%d->%s] %s current lsp0 looking at %d\n",portNumber,ep[0],seqAck.get(lsp.senderPort()),lsp.seq());
+                            if (!seqAck.containsKey(lsp.senderPort()))
+                                seqAck.put(lsp.senderPort(),-1 );
+                          
+                            if (seqAck.get(lsp.senderPort()) >= lsp.seq()) {
+                                System.out.printf("[%d->%s] Skipping LSP %d\n",portNumber,ep[0],lsp.seq());
+                                continue;
+                            }
+                            // Send the LSP
+                            System.out.printf("[%d->%s]  Sending LSP %d\n",portNumber,ep[0],lsp.seq());
+                            pw.printf("l %d %d %d", lsp.senderPort(),lsp.seq(),lsp.ttl());
+                            for (int x=0; x < lsp.adjRouterPort().size(); x++) {
+                                //pw.printf(" %d-%d",lsp.adjRouterPort()[x],lsp.distance()[x]);
+                                pw.printf(" %d-%d",lsp.adjRouterPort().get(x),lsp.distance().get(x));
+                            }
+                            seqAck.put(lsp.senderPort(),lsp.seq());
+                            pw.println();
+                            pw.flush();
+
+                          }
+                          pw.println("k"); // just send a keepalive for now, needs to come out when we do the link state packet part
+                          pw.flush();
+                            if (br.readLine().charAt(0) == 'k') { 
+                               //System.out.printf("Received Keepalive back from %d\n",port);
+                            }
+                            else {
+                                //System.out.printf("");
+                            }
                             s.close();
                         } 
                        catch (Exception IOException) {
